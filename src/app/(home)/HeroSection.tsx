68a86287da4e3c94b23db8289199debfc4b13c25
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2024-06-24 17:01:14 +0100

    remove assets

--- src/app/(home)/HeroSection.tsx
+++ src/app/(home)/HeroSection.tsx
@@ -3,7 +3,7 @@ import { SearchForm } from '@/components/search/SearchForm'
 export default function HeroSection() {
   return (
     <div
-      style={{ backgroundImage: 'url(/bg-home/bg-home-4.jpg)' }}
+      style={{ backgroundImage: 'url(/bg-home/bg-home.jpg)' }}
       className="h-screen bg-center bg-cover -mt-16 flex justify-center items-center px-4"
     >
       <SearchForm />
